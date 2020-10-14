99beccd491c1b2ba3b0981f25b229513d76f9170
Demin Alex <fizmail@inbox.ru>
2020-10-15 00:41:52 +0300

    Create nasled3_lab7.cpp

---
+++ nasled3_lab7.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Point {
+  public:
+    void setPosition(int _x, int _y);
+    void getPosition(int &_x, int &_y);
+  private:
+    int x, y;
+};
+
+class Unit { 
+  public:
+    void move();
+  private:
+    Point pt;
+};
+
+int main() {
+    Unit unit1;
+    unit1.move();
+}
+
+void Unit::move() { };
