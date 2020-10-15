bd52ccb84d6282acf583ae78a316c917c29e5137
Demin Alex <fizmail@inbox.ru>
2020-10-15 06:57:47 +0300

    Create virt_fnct_lab7.cpp

---
+++ virt_fnct_lab7.cpp
@@ -0,0 +1,176 @@
+#include <iostream>
+using namespace std;
+
+class Goods {}; 
+
+class Unit {
+  public:
+    Unit();
+    Unit(int _x, int _y, string _name);
+    ~Unit();  
+    virtual void move();
+    void getPosition(int &_x, int &_y);
+  private:
+    int x, y;  
+    string name;
+};
+
+class Unit2 {
+    // some code
+    virtual void move();
+    // more code
+};
+
+class TradeUnit : Unit {
+  public:
+    TradeUnit();	
+    ~TradeUnit();  
+    TradeUnit(int _x, int _y, string _name);  
+    void loadGoods(const Goods &gd);
+    Goods unloadGoods(int pos);
+    Goods getGoods(int pos);
+    int getGoodsCount() {return goodsCount;}
+  private:
+    int goodsCount;
+    Goods *storage;
+};
+
+enum Torders {others, guard};
+
+class MilitaryUnit : public Unit { // Так лучше
+  public:  
+    MilitaryUnit();
+    MilitaryUnit(int _x, int _y, string _name);
+    ~MilitaryUnit();  
+    void fight();
+    virtual void move();
+  protected: // Так лучше 
+    int health;
+    Torders orders;
+    void init();
+    // И еще много свойств и методов.
+};
+
+class MilitaryUnit2 : public Unit {
+    // Будет сделана реализация
+    virtual void move();
+};
+
+class Chivalry : public MilitaryUnit {
+    // …
+    virtual void move();
+    // Везде надо писать virtual
+};
+
+class Stream {
+  public:
+    void save (int);  
+    void load (int);  
+    void save(double);  
+    void load (double);
+};
+
+class NetworkStream : Stream {
+  public:
+    void save(int);
+    void load(int);
+    void save(double);
+    void load(double);
+    bool openConnection(string);
+    bool closeConnection();
+    // And so on.
+};
+
+class FileStream : Stream {
+  public:
+    void save(int);
+    void load(int);
+    void save(double);
+    void load(double);
+    bool openFile(string);
+    bool closeFile();
+    // And so on.
+};
+
+Unit::Unit() :
+    x(0), y(0), name("") { cout <<"Unit\n"; }
+    
+Unit::Unit(int _x, int _y, string _name) :
+    x(_x), y(_y), name(_name) { cout <<"Unit3\n";  }
+
+// MilitaryUnit::MilitaryUnit() :
+//     health(0) { cout <<"MilitaryUnit\n"; }  
+
+//неправильный конструктор
+// MilitaryUnit::MilitaryUnit(int _x, int _y, string _name) {
+//     cout <<"MilitaryUnit3\n";  
+//     Unit(_x, _y, _name);
+//     health=0;
+// }
+
+//правильный конструктор
+// MilitaryUnit::MilitaryUnit(int _x, int _y, string _name) :
+//     Unit(_x, _y, _name), health(0) { cout <<"MilitaryUnit3\n";  }
+
+//самый правильный конструктор
+MilitaryUnit::MilitaryUnit() { init(); }
+MilitaryUnit::MilitaryUnit(int _x, int _y, string _name) :
+    Unit(_x, _y, _name) { init(); }
+void MilitaryUnit::init() { health=0; } // Так лучше
+
+MilitaryUnit::~MilitaryUnit() { }  
+Unit::~Unit() {}
+
+void Unit::move() {
+    // Обычный юнит не передвигается
+    cout <<"Unit::move\n";
+}
+
+// Поведение войск несколько иное.
+void MilitaryUnit::move() {
+    switch(orders) {
+        case guard: // Stay here
+        ; // And so on
+    }
+    cout <<"MilitaryUnit::move\n";
+}
+
+class Battleship : MilitaryUnit {};
+
+class TheGame {
+  private:
+    // чтобы каждый объект вел себя
+    // корректно, надо знать его тип.
+    Battleship *btlships;  
+    int bshipCount;  
+    Chivalry *chivs;
+    int ChCount;
+    // И так несколько раз.
+};
+
+void main1() {
+    Unit unit;
+    MilitaryUnit *munit=new MilitaryUnit;
+
+    munit->move(); // Корректно передвигает
+    unit.move(); // Обычный юнит не передвигается 
+    ((Unit*)munit)->move(); // Аналогично.
+    //^^^^^Корректное преобразование типов.
+}
+
+void main2() {
+    //MilitaryUnit mu0;
+    MilitaryUnit mu1(10, 10, "unit1");
+    cout <<"done";
+}
+
+int main() {
+    MilitaryUnit *munit=new MilitaryUnit;  
+    Unit *unit=munit;
+    
+    munit->move(); // Корректно передвигает 
+    unit->move(); // Обычный юнит не передвигается 
+    ((MilitaryUnit *)unit)->move(); // Передвигает юнит
+    //^^^^^Корректное преобразование типов.
+}
+
