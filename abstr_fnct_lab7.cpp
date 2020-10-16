c0a7842aed4b5c19fcc39c598092fc59e5a70080
Demin Alex <fizmail@inbox.ru>
2020-10-16 10:29:26 +0300

    Update abstr_fnct_lab7.cpp

--- abstr_fnct_lab7.cpp
+++ abstr_fnct_lab7.cpp
@@ -21,10 +21,10 @@ enum Torders {others, guard};
 class MilitaryUnit : public Unit { // Так лучше
   public:  
     MilitaryUnit();
-    MilitaryUnit(int _x, int _y, string _name);
+//    MilitaryUnit(int _x, int _y, string _name);
     ~MilitaryUnit();  
     void fight();
-    virtual void move();
+   // virtual void move();
   protected: // Так лучше 
     int health;
     Torders orders;
@@ -39,7 +39,7 @@ class Chivalry : public MilitaryUnit {
         kind=ukChivalry;
     };
     // …
-    virtual void move() {cout<<"cmv\n";};
+    virtual void move() {cout<<"cmv\n";}; // реализация абстрактной ф-ции
 };
 
 
@@ -50,8 +50,15 @@ class Galley : public MilitaryUnit {
         kind=ukGalley;
     };
     // …
-    virtual void move() {cout<<"mmv\n";};
+    virtual void move() {cout<<"mmv\n";}; // реализация абстрактной ф-ции
 };
+
+
+MilitaryUnit::MilitaryUnit() {};
+MilitaryUnit::~MilitaryUnit() {};
+
+//class Galley: public Chivalry {};
+class TradeCart: public Chivalry {};
 
 
 class TheGame {
@@ -70,6 +77,42 @@ void TheGame::moveAll() {
 }
 
 int main() {
+    Chivalry chl;
+    Galley gal;
+    //Unit un; 
+    // error: cannot declare variable ‘un’ to be of abstract type ‘Unit’
+}
+
+#if 0
+
+int main() {
     TheGame game;
     game.moveAll();
 }
+
+int main() {
+    int unitCount=3;
+    Unit *units[unitCount]= {
+        new Galley,
+        new Chivalry,
+        new TradeCart
+    };
+    //    …
+    for( int i=0; i<unitCount; i++ )
+        delete units[i]; //Don’t care for type.
+}
+
+
+int main2() {
+    int unitCount=3;
+    Unit **units;
+    units=new Unit*[unitCount];
+    units[0]=new Galley;
+    units[1]=new Chivalry;
+    units[2]=new TradeCart;
+    //    …
+    for( int i=0;i<unitCount; i++ )
+    delete units[i]; //Don’t care for type.
+    delete [] units;
+}
+#endif
