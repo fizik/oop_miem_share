7f81201b6faea05eb6914a1559dd9381642c68bf
Demin Alex <fizmail@inbox.ru>
2020-10-16 05:21:04 +0300

    Update abstr_fnct_lab7.cpp

--- abstr_fnct_lab7.cpp
+++ abstr_fnct_lab7.cpp
@@ -1,3 +1,8 @@
+#include <iostream>
+using namespace std;
+
+class Goods {}; 
+
 
 enum UnitKind {ukNone, ukChivalry, ukGalley /*, …*/};
 
@@ -5,8 +10,50 @@ class Unit {
   public:
     UnitKind getKind() { return kind; };  
     virtual void move()=0;
-  private:  
+  protected:  
     UnitKind kind;
 };
 // Наследуем от Unit класс MilitaryUnit,
-//а  от него класс Chivalry и остальные
+//а от него класс Chivalry и остальные
+
+enum Torders {others, guard};
+
+class MilitaryUnit : public Unit { // Так лучше
+  public:  
+    MilitaryUnit();
+    MilitaryUnit(int _x, int _y, string _name);
+    ~MilitaryUnit();  
+    void fight();
+    virtual void move();
+  protected: // Так лучше 
+    int health;
+    Torders orders;
+    void init();
+    // И еще много свойств и методов.
+};
+
+class Chivalry : public MilitaryUnit {
+  public:
+    Chivalry() {
+        // …
+        kind=ukChivalry;
+    };
+    // …
+    virtual void move();
+};
+
+
+class Galley : public MilitaryUnit {
+  public:
+    Galley() {
+        // …
+        kind=ukGalley;
+    };
+    // …
+};
+
+
+int main() {
+    cout <<"ok\n";
+}
+    
