b36cbd645fd727bca04509cdf4566c205a8dee22
Demin Alex <fizmail@inbox.ru>
2020-10-29 15:39:46 +0300

    Update func_ptr_lec8.cpp

--- func_ptr_lec8.cpp
+++ func_ptr_lec8.cpp
@@ -29,12 +29,17 @@ int cmpMines2(const Mine &m1, const Mine &m2) {
     return(m1.getInterval() - m2.getInterval());
 }
 
-void sortMines(Mine *mines, int count, int  (*cmpFunc) (const Mine &m1, const Mine &m2) ) {
+void sortMines(Mine *mines, 
+               int count, 
+               int  (*cmpFunc) (const Mine &m1, const Mine &m2)
+              )
+{
     for( int i=0; i<count; i++ )
         for( int j=i+1; j<count; j++ )
             if(cmpFunc(mines[i],mines[j])<0)
                 swapMines(mines[i], mines[j]);
 }
+
 
 void sortMinesBy(Mine *mns, int mCount, cSortBy sortBy) {
     if( sortBy==sortByProd )
