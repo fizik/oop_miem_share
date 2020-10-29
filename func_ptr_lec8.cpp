252103a13e7377ae9748af15ea69486d723b155b
Demin Alex <fizmail@inbox.ru>
2020-10-29 16:00:16 +0300

    Update func_ptr_lec8.cpp

--- func_ptr_lec8.cpp
+++ func_ptr_lec8.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
 using namespace std;
 
-enum cSortBy {sortByProd, sortByInterval};
+struct Mine{
+    int getProduct() const{return 0;};
+    int getInterval() const{return 0;};
+};
+
+typedef int (*cmpFuncType) (const  Mine &m1, const Mine &m2);
+
+enum SortModes {smProd, smIntr, smWork};
+
+
+class MinesCollection {
+  public:
+    // Some code here  
+    void setSortingMode(SortModes mode);  
+    void sortMines();
+  private: 
+    cmpFuncType ptr;
+};
+
+int cmpMines1(const Mine &m1, const Mine &m2) {
+    return(m1.getProduct() - m2.getProduct());
+}
+
+int cmpMines2(const Mine &m1, const Mine &m2) {
+    return(m1.getInterval() - m2.getInterval());
+}
+
+void MinesCollection::setSortingMode(SortModes mode) { 
+    switch( mode ) {
+        case smProd: ptr=cmpMines1; return;  
+        case smIntr: ptr=cmpMines2; return;
+        // А еще добавить по возрастанию или убыванию.
+    }
+}
+
 
 int f1(float a, char b) {
     cout<<a<<"; "<<b<<"\n";
@@ -13,22 +47,9 @@ int f2(float a, char b) {
     return 2;
 }
 
-struct Mine{
-    int getProduct() const{return 0;};
-    int getInterval() const{return 0;};
-};
-
 void swapMines(Mine &m1, Mine &m2) {
 }
     
-int cmpMines1(const Mine &m1, const Mine &m2) {
-    return(m1.getProduct() - m2.getProduct());
-}
-
-int cmpMines2(const Mine &m1, const Mine &m2) {
-    return(m1.getInterval() - m2.getInterval());
-}
-
 void sortMines(Mine *mines, 
                int count, 
                int  (*cmpFunc) (const Mine &m1, const Mine &m2)
@@ -36,15 +57,14 @@ void sortMines(Mine *mines,
 {
     for( int i=0; i<count; i++ )
         for( int j=i+1; j<count; j++ )
-            if(cmpFunc(mines[i],mines[j])<0)
+            if(cmpFunc(mines[i], mines[j])<0)
                 swapMines(mines[i], mines[j]);
 }
 
-
-void sortMinesBy(Mine *mns, int mCount, cSortBy sortBy) {
-    if( sortBy==sortByProd )
+void sortMinesBy(Mine *mns, int mCount, SortModes sortBy) {
+    if( sortBy==smProd )
         sortMines( mns, mCount, cmpMines1);  
-    else if( sortBy==sortByInterval )
+    else if( sortBy==smIntr )
         sortMines( mns, mCount, cmpMines2);
     // and so on
 }
