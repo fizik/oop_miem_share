ac741d384cc36137dd545b4390f536aaa4d7dcfc
Demin Alex <fizmail@inbox.ru>
2020-10-29 14:20:34 +0300

    Update final_override_lec8.cpp

--- final_override_lec8.cpp
+++ final_override_lec8.cpp
@@ -8,22 +8,17 @@ struct A {
 };
 
 struct B : public A {
-    void f(int) override; // OK
-    virtual void g() const final; // OK
-    virtual void g(long); // OK: новая виртуальная функция
-};
-
-#if Wrong
-struct C : public A {
-    void sone_func() override; //Ошибка: неверное имя функции 
-    virtual void f(long) override; //Ошибка: несоответствие типа 
-    virtual int f(int) override;//Ошибка: несоответствие возв. типа
+    virtual void f(long) override;  // Ошибка: несоответствие типа парам. 
+    virtual void f(int) override;   // OK
+    virtual int  g(int) override;   // Ошибка: несоответствие возв. типа
+    virtual void g() const final;   // OK
+    virtual void g(long);           // OK: новая виртуальная функция
+    void sone_func() override; // Ошибка: неверное имя функции 
 };
 
 struct D : B {
-    virtual void g() const; //Ошибка: замещение финальной функции
+    virtual void g() const; // Ошибка: замещение финальной функции
 };
-#endif
 
 
 int main() {
