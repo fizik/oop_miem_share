aa9713b5f8b6be49f8b98179b00296cb19cf4800
Demin Alex <fizmail@inbox.ru>
2020-10-30 03:37:55 +0300

    Create overload.cpp

---
+++ overload.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+typedef int myint;
+
+int main() {
+    myint x, y, z;
+    x=10;
+    y=25;
+    z=x+y;
+
+    cout<<"Sum of "<<x<<" + "<<y<<" = " << z;
+}
