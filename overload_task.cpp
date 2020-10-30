0b84ea10dde0d5b67feb6390ec022a6a65bc5b30
Demin Alex <fizmail@inbox.ru>
2020-10-30 07:46:58 +0300

    Create overload_task.cpp

---
+++ overload_task.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+typedef int Int;
+
+int main() {
+    Int x,y;
+    
+    cin>>x;
+    cin>>y;
+    Int z=x-y;
+
+    cout << "Sub of " << x << " - " << y << " = " << z << "\n";
+}
